0d93ce34d65ebf1d91e65446273440f6ee67d944
Rohit Pandey <pandeyrohit872@gmail.com>
2024-10-28 22:29:17 +0530

    Initial commit

---
+++ blood-donation-backend/src/test/java/com/donate/BloodDonationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
